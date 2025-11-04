424b0747b808e06e214d6605df25183b47cf415b
tenzin choenyi <phurbud5@gmail.com>
2025-11-04 13:43:29 -0500

    Initial commit for teddy's Shop

---
+++ fullstackEcommerce/src/main/java/com/luv2code/springbootecommerce/FullstackEcommerceApplication.java
@@ -0,0 +1,14 @@
+package com.luv2code.springbootecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackEcommerceApplication.class, args);
+    }
+
+
+}
